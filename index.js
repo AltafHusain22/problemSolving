99baccb444df6ab62649ebd1443c39f1f10bc21b
AltafHusain22 <fiverr.contactme@gmail.com>
2023-02-07 10:57:04 +0600

    problem-4 solved

--- index.js
+++ index.js
@@ -5,42 +5,46 @@
     // this function will receive a parameter and will calculate with multiply, sum, devide and substract and finally will return the result.
     //-------------
 
-// function mindGame(number) { 
+function mindGame(number) { 
 
-    //-------------
-    // the below condition will check the data type and will return a error message .
-    //-------------
-//     if (typeof number === 'string') {
-//        return ('Error: Input should be a number, not a string.')  
+   // -------------
+    //the below condition will check the data type and will return a error message .
+   // -------------
+    if (typeof number === 'string') {
+       return ('Error: Input should be a number, not a string.')  
       
-//     } else { 
-//     let result = 0
-//     let multiply = number * 3
-//     let sum = multiply + 10
-//     let divide = sum / 2
-//     let substract = divide - 5
-//     let finalResult = result += substract
-//     return finalResult
-//     }  
-// }
+    } else { 
+    let result = 0
+    let multiply = number * 3
+    let sum = multiply + 10
+    let divide = sum / 2
+    let substract = divide - 5
+    let finalResult = result += substract
+    return finalResult
+    }  
+}
 
 //problem -2
 
-// function evenOdd(inputStr) { 
+  //-------------
+    // this function will receive a parameter and will get a string and will find the even or odd based on input charecters
+    //-------------
 
-//     if (typeof inputStr === 'number') {
-//        return ('Error: Input should be a string, not a number.')  
+function evenOdd(inputStr) { 
+
+    if (typeof inputStr === 'number') {
+       return ('Error: Input should be a string, not a number.')  
       
-//     } else {
-//     let length = inputStr.length  // this line will get the total index of string
-//     if (length % 2 == 0) {       // this condition will check if this value is even 
-//         return ('Even')
-//     } else { 
-//         return ('Odd')
-//     }
-//     }
+    } else {
+    let length = inputStr.length  // this line will get the total index of string
+    if (length % 2 == 0) {       // this condition will check if this value is even 
+        return ('Even')
+    } else { 
+        return ('Odd')
+    }
+    }
 
-// }
+}
 
 //problem -3
 
@@ -55,7 +59,30 @@ function isLGSeven(number) { // this function will take a parameter and calculte
     
 }
 
+//problem -4
 
+    //-------------
+    // this function will receive a parameter as array and will findout the bad data (negative value) from array
+    //-------------
+function findingBadData(arr) { 
+  if (typeof arr === 'string') {
+       return ('Error: Input should be a number, not a string.')  
+      
+  } else {
+      let badData = 0
+      let goodData = 0
+      
+    for (let i = 0; i < arr.length; i++) { // get the total elements from array
+        if (arr[i] < 0 ) { // if the value is less than 0 it will store negative value on badData
+            badData++
+        } else if (arr >= 0){ 
+            goodData++
+        }   
+    }
+    return badData
+    }
+  
+}
 
 
 
